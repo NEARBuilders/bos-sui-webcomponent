42849d6d953b0c2bc26c62ce67e73e805a41802a
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2024-05-31 09:53:34 -0500

    fix account

--- src/components/SuiSigner.js
+++ src/components/SuiSigner.js
@@ -1,9 +1,12 @@
+import { useSignPersonalMessage } from "@mysten/dapp-kit";
 import React from "react";
 
 function SuiSigner(props) {
   const { provides } = props;
 
-  return <>{provides && provides({})}</>;
+  const { mutate: signPersonalMessage } = useSignPersonalMessage();
+
+  return <>{provides && provides({ signPersonalMessage })}</>;
 }
 
 export default SuiSigner;
